5134a2e3a09701bcc99c94ecb9bf5a09a3e0ef72
waltersantos-muniz <walter.ms.santos41@gmail.com>
2019-08-01 12:06:01 -0300

    segundo commit teste

--- src/projeto/git/principal.java
+++ src/projeto/git/principal.java
@@ -7,6 +7,7 @@ public class principal {
    
     public static void main(String[] args) {
         System.out.println("Olá mundo");
+        System.out.println("46545645454854856");
     }
     
 }
